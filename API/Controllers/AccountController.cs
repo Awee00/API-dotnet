3e535117fd229f5ed0d7831d8a37978141dd883a
Wahyu Awee <ardawardana70@gmail.com>
2023-04-21 04:18:40 +0700

    Add Controller dan Repository

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,133 @@
+﻿using API.Models;
+using API.Repositories.Data;
+using API.Repositories.Interface;
+using API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountRepository _accountRepository;
+
+    public AccountController(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+    [HttpGet]
+    public ActionResult GetAll()
+    {
+        var accounts = _accountRepository.GetAll();
+        return Ok(new ResponseDataVM<IEnumerable<Account>>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "Success",
+            Data = accounts
+        });
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetById(string id)
+    {
+        var account = _accountRepository.GetById(id);
+        if (account == null)
+            return NotFound(new ResponseErrorsVM<string>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Status = HttpStatusCode.NotFound.ToString(),
+                Errors = "Id Not Found"
+            });
+        return Ok(new ResponseDataVM<Account>
+        {
+            Code = StatusCodes.Status200OK,
+            Status = HttpStatusCode.OK.ToString(),
+            Message = "Success",
+            Data = account
+        });
+    }
+
+    [HttpPost]
+    public ActionResult Insert(Account account)
+    {
+        if (account.EmployeeNIK == "" || account.Password == "")
+        {
+            return BadRequest(new ResponseErrorsVM<string>
+            {
+                Code = StatusCodes.Status400BadRequest,
+                Status = HttpStatusCode.BadRequest.ToString(),
+                Errors = "Value Cannot be Null or Default"
+            });
+        }
+
+        var insert = _accountRepository.Insert(account);
+        if (insert > 0)
+            return Ok(new ResponseDataVM<Account>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Insert Success",
+                Data = null!
+            });
+        return BadRequest(new ResponseErrorsVM<string>
+        {
+            Code = StatusCodes.Status500InternalServerError,
+            Status = HttpStatusCode.InternalServerError.ToString(),
+            Errors = "Insert Failed / Lost Connection"
+        });
+    }
+
+    [HttpPut]
+    public ActionResult Update(Account account)
+    {
+        if (account.Password == "")
+        {
+            return BadRequest(new ResponseErrorsVM<string>
+            {
+                Code = StatusCodes.Status400BadRequest,
+                Status = HttpStatusCode.BadRequest.ToString(),
+                Errors = "Value Cannot be Null or Default"
+            });
+        }
+
+        var update = _accountRepository.Update(account);
+        if (update > 0)
+            return Ok(new ResponseDataVM<Account>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Insert Success",
+                Data = null!
+            });
+        return BadRequest(new ResponseErrorsVM<string>
+        {
+            Code = StatusCodes.Status500InternalServerError,
+            Status = HttpStatusCode.InternalServerError.ToString(),
+            Errors = "Insert Failed / Lost Connection"
+        });
+    }
+
+    [HttpDelete]
+    public ActionResult Delete(string id)
+    {
+        var delete = _accountRepository.Delete(id);
+        if (delete > 0)
+            return Ok(new ResponseDataVM<Account>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Delete Success",
+                Data = null!
+            });
+        return BadRequest(new ResponseErrorsVM<string>
+        {
+            Code = StatusCodes.Status500InternalServerError,
+            Status = HttpStatusCode.InternalServerError.ToString(),
+            Errors = "Insert Failed / Lost Connection"
+        });
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Context;
+using API.Repositories.Data;
+using API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,15 @@ builder.Services.AddControllers();
 // Add DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(connectionString));
+
+// Add Depedency Injection for Repository
+builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
+builder.Services.AddScoped<IEducationRepository, EducationRepository>();
+builder.Services.AddScoped<IProfilingRepository, ProfilingRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
